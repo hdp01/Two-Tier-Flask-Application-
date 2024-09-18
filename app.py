752cd2f454410ee5489b8e1a07d08703d3c2bbad
Harsh Pashine <pashineharsh01@gmail.com>
2024-09-18 19:50:47 +0530

    Initial commit: Flask app

---
+++ app.py
@@ -0,0 +1,15 @@
+# app.py
+from flask import Flask
+
+app = Flask(__name__)
+
+@app.route('/')
+def home():
+    return "Hello, World! Welcome to my Flask App!"
+
+@app.route('/about')
+def about():
+    return "This is a simple Flask app."
+
+if __name__ == '__main__':
+    app.run(host='0.0.0.0', port=5000)
